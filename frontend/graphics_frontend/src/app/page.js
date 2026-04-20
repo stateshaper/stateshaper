ffbd464822746dda6784e30c1f48dacaff41b0e2
stateshaper <jdunn7008@gmail.com>
2026-04-20 16:32:25 -0600

    Add conditional rendering for DesktopOnly component

--- frontend/graphics_frontend/src/app/page.js
+++ frontend/graphics_frontend/src/app/page.js
@@ -182,9 +182,11 @@ export default function Home() {
         `}
       </style>
       <div className="grid grid-rows-1 place-items-center text-3xl mt-8 text-gray-200 font-bold">
+        <div className={DesktopOnly == true ? "grid place-items-center text-lg" : ""}>
         <div>
           Stateshaper Graphics Demo
         </div>   
+        </div>
       </div>
       {DesktopOnly == true ? 
         <div className="grid place-items-center">
