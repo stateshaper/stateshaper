dd26cc8be6429f75867e18853eedfef0c685292d
stateshaper <jdunn7008@gmail.com>
2026-05-04 12:14:29 -0600

    Fix grid height in page.js for shape display

--- frontend/graphics_frontend/src/app/page.js
+++ frontend/graphics_frontend/src/app/page.js
@@ -230,7 +230,7 @@ export default function Home() {
                       return (
                         <div
                           key={index}
-                          className="grid grid-rows-1 grid-cols-5 gap-2 md:gap-4 w-full h-8 place-items-center"
+                          className="grid grid-rows-1 grid-cols-5 gap-2 md:gap-4 w-8 h-8 place-items-center"
                         >
                           {row_shapes.map((shape, cols) => (
                             <div key={cols} className={shape}>
