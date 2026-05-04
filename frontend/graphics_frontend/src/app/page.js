a01891782c4a0d47c102a345f8dd4e29ff887f5a
stateshaper <jdunn7008@gmail.com>
2026-05-04 13:21:56 -0600

    Refactor mouse event handlers and conditional rendering

--- frontend/graphics_frontend/src/app/page.js
+++ frontend/graphics_frontend/src/app/page.js
@@ -309,20 +309,20 @@ export default function Home() {
 {Data ?
   <div>
 
-          <div className={!ShowCode ? "text-white text-xl hover:font-bold bottom-4 right-48 md:bottom-6 md:right-192 ml-auto absolute hover:text-gray-300 cursor-pointer" : "text-xl font-bold bottom-4 right-48 md:bottom-6 md:right-192 ml-auto absolute text-gray-300 cursor-pointer"} onMouseEnter={!ShowExample ? e=>setShowCode(true) : e=>setShowExample(false)} onClick={e=>setShowCode(false)}>
+          <div className={!ShowCode ? "text-white text-xl hover:font-bold bottom-4 right-48 md:bottom-6 md:right-192 ml-auto absolute hover:text-gray-300 cursor-pointer" : "text-xl font-bold bottom-4 right-48 md:bottom-6 md:right-192 ml-auto absolute text-gray-300 cursor-pointer"} onMouseEnter={ShowExample == false ? e=>setShowCode(true) : e=>setShowExample(false)} onClick={e=>setShowCode(false)}>
             CODE
           </div>
-          <div className="text-white text-xl hover:font-bold bottom-4 left-48 md:bottom-6 md:right-12 md:left-auto ml-auto absolute hover:text-gray-300 cursor-pointer" onMouseEnter={!ShowCode ? e=>setShowExample(true) : e=>setShowCode(false)} onClick={e=>setShowExample(false)}>
+          <div className="text-white text-xl hover:font-bold bottom-4 left-48 md:bottom-6 md:right-12 md:left-auto ml-auto absolute hover:text-gray-300 cursor-pointer" onMouseEnter={ShowCode == false ? e=>setShowExample(true) : e=>setShowCode(false)} onClick={e=>setShowExample(false)}>
             EXAMPLE ONLY
           </div>
-          {ShowCode && !ShowExample?
+          {ShowCode?
             <div className="text-white p-4 py-5 bottom-16 right-54 md:bottom-18 md:right-192 ml-auto absolute w-auto md:w-128 h-auto rounded-lg bg-blue-600">
               <div className="text-md ">
                 <span className="font-bold">Frontend:</span> <a className="cursor-pointer hover:text-gray-300 hover:italic" href="https://www.github.com/jgddesigns/stateshaper/tree/graphics_demo" target="_blank">https://www.github.com/jgddesigns/stateshape/tree/graphics_demo</a>
               </div>
             </div>
           : null}
-          {ShowExample && !ShowCode?
+          {ShowExample?
             <div className="text-white p-4 bottom-16 left-54 md:bottom-18 md:right-12 md:left-auto ml-auto absolute w-auto md:w-128 h-auto rounded-lg bg-blue-600">
               <div className="text-lg font-bold">
                 Sample app, real logic. 
